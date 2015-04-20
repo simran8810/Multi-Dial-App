9e54fd74c1b77d982ce63eee34562411ea8ca9cb
simran8810 <simran.kaur@hindustantimes.com>
2015-04-20 11:20:31 +0530

    First Commit

---
+++ app/src/main/java/com/example/multi_dial_app/CallActivity.java
@@ -0,0 +1,99 @@
+package com.example.multi_dial_app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+/**
+ * Created by dhruv on 24/3/15.
+ */
+public class CallActivity extends Activity {
+
+
+    private final Integer RESCODE = 1;
+
+    private Button contBtn;
+
+    private Button stopBtn;
+
+    private String message;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_second);
+
+
+
+        String url = getIntent().getStringExtra("URLKEY");
+
+
+        Log.d("url",url);
+
+
+        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse(url));
+
+        startActivity(intent);
+
+        contBtn = (Button)findViewById(R.id.continueCalling);
+        stopBtn = (Button)findViewById(R.id.stopCalling);
+
+        contBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                message = "CONTINUE";
+
+                //returning result
+                Intent intent1 = new Intent();
+                intent1.putExtra("MESSAGE", message);
+                setResult(RESCODE, intent1);
+                finish();//finishing activity
+
+
+            }
+        });
+
+        stopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                message = "STOP";
+
+                //returning result
+                Intent intent1= new Intent();
+                intent1.putExtra("MESSAGE",message);
+                setResult(RESCODE,intent1);
+                finish();//finishing activity
+
+            }
+        });
+
+
+
+    }
+
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_doIt:
+                if (checked)
+                    Toast.makeText(getApplicationContext(),"yes checked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.radio_dontDoIt:
+                if (checked)
+                    Toast.makeText(getApplicationContext(),"no checked", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/multi_dial_app/MainActivity.java
@@ -0,0 +1,165 @@
+package com.example.multi_dial_app;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.app.Activity;
+import android.content.Intent;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import java.util.ArrayList;
+
+
+public class MainActivity extends Activity {
+
+    private ArrayList<String> numbers;
+
+    private Integer i =0;
+
+    private final Integer RESCODE = 1;
+
+    private int prevState = TelephonyManager.CALL_STATE_IDLE;
+
+    private int currentState;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        numbers = new ArrayList<>();
+
+        numbers.add("tel:111111");
+        numbers.add("tel:22222");
+        numbers.add("tel:33333");
+
+
+        //TelephonyManager TelephonyMgr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+
+        Button btn = (Button)findViewById(R.id.startCalling);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //while(i<numbers.size())
+                //{
+                makeACall();
+
+                //}
+
+            }
+        });
+
+
+
+        //TelephonyMgr.listen(new TeleListener(),
+        //          PhoneStateListener.LISTEN_CALL_STATE);
+
+
+
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    /*
+        class TeleListener extends PhoneStateListener {
+            public void onCallStateChanged(int state, String incomingNumber) {
+                super.onCallStateChanged(state, incomingNumber);
+
+
+
+                switch (state) {
+                    case TelephonyManager.CALL_STATE_IDLE:
+                        // CALL_STATE_IDLE;
+
+                        break;
+                    case TelephonyManager.CALL_STATE_OFFHOOK:
+                        // CALL_STATE_OFFHOOK;
+
+                        break;
+                    case TelephonyManager.CALL_STATE_RINGING:
+                        // CALL_STATE_RINGING
+
+                        break;
+                    default:
+                        break;
+                }
+
+
+                currentState = state;
+
+
+            }
+
+        }
+
+
+
+        public void check()
+        {
+            if((currentState == TelephonyManager.CALL_STATE_IDLE) && (prevState != TelephonyManager.CALL_STATE_IDLE))
+            {
+                if(i<numbers.size())
+                    makeACall();
+                else
+                    finish();
+            }
+
+            prevState = currentState;
+
+        }
+    */
+    public void makeACall()
+    {
+
+        String url =  numbers.get(i);
+
+        Log.d("mainurl",url);
+
+        i++;
+
+        Intent intent = new Intent(getApplicationContext(), CallActivity.class);
+
+        intent.putExtra("URLKEY",url);
+
+        // startActivity(intent);
+
+        startActivityForResult(intent, RESCODE);
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+
+        if (requestCode == RESCODE) {
+            // Make sure the request was successful
+            String message = data.getStringExtra("MESSAGE");
+
+
+            if (message.equals("STOP")) {
+
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                //now no more calls will be made, otherwise let it pass
+            }
+            else if(message.equals("CONTINUE"))
+            {
+                if(i<numbers.size()) {
+                    makeACall();
+                }
+                else
+                    i=0;
+            }
+
+        }
+
+    }
+
+}
